c33de55bdcf6e1dbe240b362515e4f1e17351437
wukongO-O <l.shenglan@gmail.com>
2022-09-18 17:18:11 -0400

    add function to convert between C and F

--- weather.js
+++ weather.js
@@ -1,4 +1,5 @@
-//need to add try/catch for location error + loading function
+//need to fix: - between min&max; 
+
 async function getWeatherData() {
     const searchLocation = document.querySelector('#location').value;
     const responseWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchLocation}&appid=2d02b7a50b9aefda383eb9a9a9b88f62`, {mode:'cors'});
@@ -41,26 +42,70 @@ function weatherLogo(wData) {
     }
 }
 
+function showCurrentWeather(wData) {
+    document.querySelector('.place').textContent = wData.name;
+    const weatherDes = wData.weather[0].description
+    document.querySelector('.weather').textContent = weatherDes.charAt(0).toUpperCase() + weatherDes.slice(1);
+    weatherLogo(wData);
+    document.querySelector('.temp.now').textContent = tempKtoF(wData.main.temp);
+    document.querySelector('.feelIcon').innerHTML = `
+    <img src="./imgs/thermometer.svg"> Feels like
+    `;
+    document.querySelector('.temp.feel').textContent = tempKtoF(wData.main.feels_like);
+    document.querySelector('.minTemp').textContent = 'Low: ';
+    document.querySelector('.l').textContent = tempKtoF(wData.main.temp_min);
+    document.querySelector('.maxTemp').textContent = 'High: ';
+    document.querySelector('.h').textContent = tempKtoF(wData.main.temp_max);
+    document.querySelector('.tDate').textContent = new Date().toDateString();
+    document.querySelector('.tNow').textContent = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    //document.querySelector('.wind').textContent = weatherInfo.wind.speed + 'm/s';
+}
+
 function displayWeather() {
     loadingWeather();
 
     getWeatherData()
         .then (weatherInfo => {
         hideLoading();
-        document.querySelector('.place').textContent = weatherInfo.name;
-        document.querySelector('.today').textContent = new Date().toDateString();
-        document.querySelector('.temp').textContent = tempKtoF(weatherInfo.main.temp);
-        document.querySelector('.weather').textContent = weatherInfo.weather[0].description;
-        weatherLogo(weatherInfo);
-        document.querySelector('.hl').textContent = tempKtoF(weatherInfo.main.temp_min) + ' - ' + tempKtoF(weatherInfo.main.temp_max);
-        document.querySelector('.wind').textContent = weatherInfo.wind.speed + 'm/s';
+        showCurrentWeather(weatherInfo);
         }).catch(() => {
-            document.querySelector('.place').textContent = 'Oops, location not found';
+            document.querySelector('.err').textContent = 'Oops, location not found';
         })
 
     document.querySelector('#location').value = '';
 }
-
+    
 const searchBtn = document.querySelector('#searchBtn');
 searchBtn.addEventListener('click', displayWeather);
 
+function tempFtoC() {
+    const tempFs = document.querySelectorAll('.temp');
+    console.log(tempFs);
+    tempFs.forEach(temp => {
+        const tempFNumber = temp.textContent.split(' ')[0];
+        const tempC = Math.round((tempFNumber - 32)*5/9);
+        temp.textContent = tempC + ' °C';
+    });
+    document.querySelector('.convertTemp').textContent = 'Display °F';
+}
+function tempCtoF() {
+    const tempCs = document.querySelectorAll('.temp');
+    console.log(tempCs);
+    tempCs.forEach(t => {
+        const tempCNumber = t.textContent.split(' ')[0];
+        const tempF = Math.round(tempCNumber*9/5 + 32);
+        t.textContent = tempF + ' °F'
+    });
+    document.querySelector('.convertTemp').textContent = 'Display °C';
+}
+const convertTemp = document.querySelector('.convertTemp');
+let counter = 0;
+convertTemp.addEventListener('click', () => {
+    counter +=1;
+    if (counter === 1) {
+        tempFtoC();
+    } else if (counter === 2) {
+        tempCtoF();
+        return counter = 0;
+    }; 
+});
